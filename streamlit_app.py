c7af2fbb4c4b5214665f26dfc42243fad77088bc
angross75 <149438349+angross75@users.noreply.github.com>
2025-05-14 18:49:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,22 @@
 import streamlit as st
 import pandas as pd
-import csv
+import txt
 import io
 
 st.title("Avid Locator Generator")
 
 # Upload del file
-uploaded_file = st.file_uploader("Carica un file CSV o Excel con 'timecode' e 'commento'", type=["txt", "pdf"])
+uploaded_file = st.file_uploader("Carica un file txt o Pdf con 'timecode' e 'commento'", type=["txt", "pdf"])
 
 # Input del Clip Name
 clip_name = st.text_input("Clip Name", value="Mod Angelo")
 
 if uploaded_file and clip_name:
     # Lettura file
-    if uploaded_file.name.endswith(".csv"):
-        df = pd.read_csv(uploaded_file)
+    if uploaded_file.name.endswith(".txt"):
+        df = pd.read_txt(uploaded_file)
     else:
-        df = pd.read_excel(uploaded_file)
+        df = pd.read_pdf(uploaded_file)
 
     # Controllo colonne
     if "timecode" in df.columns and "commento" in df.columns:
@@ -25,7 +25,7 @@ if uploaded_file and clip_name:
 
         # Generazione del file
         output = io.StringIO()
-        writer = csv.writer(output, delimiter="\t")
+        writer = txt.writer(output, delimiter="\t")
         for _, row in df.iterrows():
             writer.writerow([clip_name, row["timecode"], "V1", "RED", row["commento"], 1])
         st.download_button(
